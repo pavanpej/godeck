97d2be9d66501eb4d53acb56d75ddfadd8968fb8
Pavan Rao <pavanpej@gmail.com>
2020-02-15 09:59:03 +0000

    Initial commit

---
+++ deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+	suits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	values := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range suits {
+		for _, value := range values {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
