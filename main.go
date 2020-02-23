3e61b500bb608b44a538046c6c2c14b437646742
Pavan Rao <pavanpej@gmail.com>
2020-02-23 21:21:17 +0530

    add map definitions

--- main.go
+++ main.go
@@ -3,7 +3,18 @@ package main
 import "main/deck"
 
 func main() {
+	// deck code
 	cards := deck.NewDeckFromFile("my_cards")
 	cards.Shuffle()
 	cards.Print()
+
+	// map definitions
+	// colors := map[string]string{
+	// 	"red":   "#ff000",
+	// 	"green": "#00ff00",
+	// 	"blue":  "#0000ff",
+	// }
+	// var colors map[string]string
+	// colors := make(map[string]string)
+
 }
